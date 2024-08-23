819210425765c0cd53baf708b90222b71c7f2e5b
Rafael Fagundes <rafaelcflima@gmail.com>
2024-08-23 14:54:33 -0400

    Add footer and home done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./About";
 import "./App.css";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import NavBar from "./NavBar";
 import Specials from "./Specials";
@@ -14,63 +15,7 @@ function App() {
         <Specials></Specials>
         <Testimonials></Testimonials>
         <About></About>
-
-        <footer>
-          <div className="container">
-            <div className="logo">
-              <img src="logo.svg" alt="Little Lemon Logo"></img>
-            </div>
-            <div className="navigation">
-              <h3>Navigation</h3>
-              <ul>
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Menu</a>
-                </li>
-                <li>
-                  <a href="#">Testimonials</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-              </ul>
-            </div>
-            <div className="contact">
-              <h3>Contact</h3>
-              <p>12345, Citrus Avenue Chicago, IL 60611</p>
-              <p>contact@littlelemonchicago.com</p>
-              <p>(312) 555-7890</p>
-            </div>
-            <div className="social-media">
-              <h3>Social Media</h3>
-              <ul>
-                <li>
-                  <a href="#">
-                    <img src="instagram.svg" alt="Instagram"></img>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="pinterest.svg" alt="Pinterest"></img>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="tiktok.svg" alt="TikTok"></img>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <img src="facebook.svg" alt="Facebook"></img>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <p className="copyright">© 2024. All rights reserved.</p>
-        </footer>
+        <Footer></Footer>
       </body>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-row">
+          <div className="logo">
+            <img src="img/footer-logo.png" alt="Little Lemon Logo"></img>
+          </div>
+          <div className="footer-links">
+            <div className="navigation">
+              <h3 className="serif secondary-text-color">Navigation</h3>
+              <ul>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#menu">Menu</a>
+                </li>
+                <li>
+                  <a href="#testimonials">Testimonials</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+              </ul>
+            </div>
+            <div className="contact">
+              <h3 className="serif secondary-text-color">Contact</h3>
+              <p>12345, Citrus Avenue Chicago, IL 60611</p>
+              <p>contact@littlelemonchicago.com</p>
+              <p>(312) 555-7890</p>
+            </div>
+            <div className="social-media">
+              <h3 className="serif secondary-text-color">Social Media</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img src="/img/instagram.svg" alt="Instagram"></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="/img/threads.svg" alt="Threads"></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="/img/tiktok.svg" alt="TikTok"></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="/img/facebook.svg" alt="Facebook"></img>
+                  </a>
+                </li>
+              </ul>
+              <p className="copyright">© 2024. All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
